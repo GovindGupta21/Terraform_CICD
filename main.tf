70380c01afe9771c4f4eedc83be568328cbe5814
GovindGupta21 <govindarajumurarishetty@gmail.com>
2025-03-04 12:22:43 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-0c50b6f7dc3701ddd"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "dev123"
+    }
+}
